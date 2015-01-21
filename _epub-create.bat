526d25f91ddaf9d4749ba16601e64f4af547a852
Cyril Lamotte <lamotte.cyril@gmail.com>
2015-01-21 09:35:25 +0100

    Update _epub-create

--- _epub-create.bat
+++ _epub-create.bat
@@ -1,1 +1,5 @@
-7z a -tZip ebook.epub mimetype OEBPS META-INF
+@echo off
+for %%a in (.) do set currentfolder=%%~na
+
+7z a -tZip -mx0 %currentfolder%.epub mimetype
+7z a -tZip %currentfolder%.epub OEBPS META-INF
